5e5c26b6db0a40b63d75bfd91859de670356af20
dusti <dusti@DESKTOP-8A36QUM>
2017-08-31 18:08:58 -0400

    Add project files.

---
+++ BarApp1/Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BarApp1.Models;
+
+namespace BarApp1.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Index()
+        {
+            var menuList = new List<drinks>{
+                new drinks() { beverage = "Beer", cost = 5 } ,
+                new drinks() { beverage = "Wine", cost = 6 } ,
+                new drinks() { beverage = "Apple Martini", cost = 8 } ,
+                        };
+
+            return View(menuList);
+        }
+    }
+}
---
+++ BarApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarApp1.Startup))]
+namespace BarApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
